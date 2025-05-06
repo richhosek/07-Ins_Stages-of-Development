d59e604537b6bcc044bbc2618b9126982cf793b0
Rich Hosek <richhosek@gmail.com>
2025-05-05 20:38:55 -0500

    Changed name of game

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1 className="m-0">Word Guess</h1>
+        <h1 className="m-0">Word Guess Master</h1>
         <p className="m-0">Guess the movie title!</p>
       </div>
     </header>
